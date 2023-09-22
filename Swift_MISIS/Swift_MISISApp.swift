fa663a5e1a3c8340cdf3cc97c51b0291f24d1e0d
Semyon Zhidkov <gamesclub57rus@gmail.com>
2023-09-22 10:21:21 +0300

    Initial Commit

---
+++ Swift_MISIS/Swift_MISISApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_MISISApp.swift
+//  Swift_MISIS
+//
+//  Created by Семён Жидков on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_MISISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
